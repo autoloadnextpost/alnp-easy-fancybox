a82424b6c46b00aa8ea0a73b928274ba94e13b62
Sébastien Dumont <mailme@sebastiendumont.com>
2019-03-06 17:23:42 +0100

    Initial commit

---
+++ includes/admin/alnp-easy-fancybox-admin.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Auto Load Next Post: Easy Fancybox - Admin.
+ *
+ * @since    1.0.0
+ * @author   Sébastien Dumont
+ * @category Admin
+ * @package  Auto Load Next Post: Easy Fancybox/Admin
+ * @license  GPL-2.0+
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'ALNP_Easy_Fancybox_Admin' ) ) {
+
+	class ALNP_Easy_Fancybox_Admin {
+
+		/**
+		 * Constructor
+		 *
+		 * @access public
+		 */
+		public function __construct() {
+			add_action( 'admin_init', array( $this, 'includes' ) );
+			add_filter( 'plugin_row_meta', array( $this, 'plugin_row_meta'), 10, 3 );
+		}
+
+		/**
+		 * Include any classes we need within admin.
+		 *
+		 * @access public
+		 */
+		public function includes() {
+			include( dirname( __FILE__ ) . '/class-alnp-easy-fancybox-check.php' );
+			include( dirname( __FILE__ ) . '/class-alnp-easy-fancybox-feedback.php' );
+		}
+
+		/**
+		 * Plugin row meta links
+		 *
+		 * @access public
+		 * @param  array  $links Plugin Row Meta
+		 * @param  string $file  Plugin Base file
+		 * @param  array  $data  Plugin Information
+		 * @return array  $links
+		 */
+		public function plugin_row_meta( $links, $file, $data ) {
+			if ( $file == plugin_basename( ALNP_ADDON_PLUGIN_FILE ) ) {
+				$links[ 1 ] = sprintf( __( 'Developed By %s', 'alnp-easy-fancybox' ), '<a href="' . $data[ 'AuthorURI' ] . '" aria-label="' . esc_attr__( 'View the Developers Site', 'alnp-easy-fancybox' ) . '">' . $data[ 'Author' ] . '</a>' );
+
+				$row_meta = array(
+					'community' => '<a href="' . esc_url( ALNP_ADDON_SUPPORT_URL ) . '" aria-label="' . esc_attr__( 'Get Support from the Community', 'alnp-easy-fancybox' ). '" target="_blank">' . esc_attr__( 'Community Support', 'alnp-easy-fancybox' ) . '</a>',
+					'review' => '<a href="' . esc_url( ALNP_ADDON_REVIEW_URL ) . '" aria-label="' . esc_attr( __( 'Review Auto Load Next Post: Easy Fancybox on WordPress.org', 'alnp-easy-fancybox' ) ) . '" target="_blank">' . __( 'Leave a Review', 'alnp-easy-fancybox' ) . '</a>',
+				);
+
+				$links = array_merge( $links, $row_meta );
+			}
+
+			return $links;
+		}
+
+	}
+
+}
+
+return new ALNP_Easy_Fancybox_Admin();
---
+++ includes/admin/class-alnp-easy-fancybox-check.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Auto Load Next Post: Easy Fancybox - Check for Auto Load Next Post Notice.
+ *
+ * @since    1.0.0
+ * @author   Sébastien Dumont
+ * @category Admin
+ * @package  Auto Load Next Post: Easy Fancybox/Admin/Check
+ * @license  GPL-2.0+
+ */
+
+// Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'ALNP_Easy_Fancybox_Check' ) ) {
+
+	class ALNP_Easy_Fancybox_Check {
+
+		/**
+		 * Checks if Auto Load Next Post is installed.
+		 *
+		 * @access public
+		 */
+		public function __construct() {
+			if ( ! defined( 'AUTO_LOAD_NEXT_POST_VERSION' ) || version_compare( AUTO_LOAD_NEXT_POST_VERSION, ALNP_ADDON_ALNP_REQUIRED, '<' ) ) {
+				add_action( 'admin_notices', array( $this, 'alnp_not_installed' ) );
+				return false;
+			}
+		}
+
+		/**
+		 * Auto Load Next Post is Not Installed Notice.
+		 *
+		 * @access public
+		 * @since  1.0.0
+		 * @return void
+		 */
+		public function alnp_not_installed() {
+			include( dirname( __FILE__ ) . '/views/html-notice-alnp-not-installed.php' );
+		}
+
+	} // END class.
+
+} // END if class exists.
+
+return new ALNP_Easy_Fancybox_Check();
